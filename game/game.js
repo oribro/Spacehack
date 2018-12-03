6343610e6418ba8a9ce54d513505446642d361e2
uvgames <rzyuval@gmail.com>
2018-12-03 16:58:31 +0200

    Update game.js

--- game/game.js
+++ game/game.js
@@ -12,23 +12,39 @@ const GROUND 	= '#';
 /* Create the board and fill environment */
 var i, j, biDigI, biDigJ;
 for(i = 0; i < HEIGHT; i++) {
-	biDigI = ("0" + i).slice(-2);
+	biDigI = getTwoDigits(i);
 	var div = document.createElement("div");
 	div.setAttribute("id","r"+biDigI);
 	document.getElementById("game").appendChild(div);
 	for(j = 0; j < WIDTH; j++) {
-		biDigJ = ("0" + j).slice(-2);
+		biDigJ = getTwoDigits(j);
 		var span = document.createElement("span");
 		span.setAttribute("id","c"+biDigI+biDigJ);
 		document.getElementById("r"+biDigI).appendChild(span);
-		document.getElementById("c"+biDigI+biDigJ).innerHTML = "#";
+		document.getElementById("c"+biDigI+biDigJ).innerHTML = GROUND;
 	}
 }
 
-// Places the character at the top left.
-document.getElementById("c0000").innerHTML = "@";
+var character = {
+	x: 0,
+	y: 0,
+	
+	get xPos() {
+		return this.x;
+	},
+	get yPos() {
+		return this.y;
+	},
+	set xPos(n) {
+		this.xPos = n;
+	},
+	set yPos(n) {
+		this.yPos = n;
+	}
+};
 
-// TODO: create character position var.
+// Places the character at the top left.
+moveChar(0,0);
 
 // Temp. movement event.
 document.body.onkeydown = function(event) {move(event)}
@@ -40,27 +56,59 @@ function move(event) {
 	
 	switch(key) {
 		case "ArrowRight":
-			// TODO: check boundaries.
-			// TODO: set current character cell to ground symbol.
-			// TODO: set cell to the right to character symbol.
-			// TODO: set character position var to new position.
+			if(checkBounds(character.xPos + 1, character.yPos)) {
+				moveChar(character.xPos + 1, character.yPos);
+			}
 			break;
 		case "ArrowLeft":
-			// TODO: check boundaries.
-			// TODO: set current character cell to ground symbol.
-			// TODO: set cell to the left to character symbol.
-			// TODO: set character position var to new position.
+			if(checkBounds(character.xPos - 1, character.yPos)) {
+				moveChar(character.xPos - 1, character.yPos);
+			}
 			break;
 		case "ArrowUp":
-			// TODO: check boundaries.
-			// TODO: set current character cell to ground symbol.
-			// TODO: set cell above to character symbol.
-			// TODO: set character position var to new position.
+			if(checkBounds(character.xPos, character.yPos - 1)) {
+				moveChar(character.xPos, character.yPos - 1);
+			}
+			break;
 		case "ArrowDown":
-			// TODO: check boundaries.
-			// TODO: set current character cell to ground symbol.
-			// TODO: set cell below to character symbol.
-			// TODO: set character position var to new position.
+			if(checkBounds(character.xPos, character.yPos + 1)) {
+				moveChar(character.xPos, character.yPos + 1);
+			}
+			break;
+		default:
+			break;
 	}
+}
+
+/* Returns true if given position is in bounds, false otherwise */
+function checkBounds(xPos, yPos) {
+	if(xPos < 0 || xPos > (WIDTH - 1)) {
+		return false;
+	}
+	if(yPos < 0 || yPos > (HEIGHT - 1)) {
+		return false;
+	}
+	return true;
+}
+
+/* Redraws and sets character at new position */
+function moveChar(xPos, yPos) {
+	var biDigX = getTwoDigits(xPos);
+	var biDigY = getTwoDigits(yPos);
+	var biDigCurX = getTwoDigits(character.xPos);
+	var biDigCurY = getTwoDigits(character.yPos);
+	
+	// Set current character cell to ground symbol and new cell to character symbol.
+	document.getElementById("c"+biDigCurY+biDigCurX).innerHTML = GROUND;
+	document.getElementById("c"+biDigY+biDigX).innerHTML = CHARACTER;
+	
+	// Set character position properties to new position.
+	character.xPos = xPos;
+	character.yPos = yPos;
+}
+
+/* Returns a 2 digit number of a 1-2 digits number */
+function getTwoDigits(n) {
+	return ("0" + n).slice(-2);
 }
 
