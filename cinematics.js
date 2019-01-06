b667c3db56fab9ddecbaedd70fd0ffde0edb7941
Ori Broda <ori.broda@mail.huji.ac.il>
2019-01-06 23:46:44 +0200

    Fixed bug where Skip button stayed after load game where cinematic had ended.

--- cinematics.js
+++ cinematics.js
@@ -4,6 +4,8 @@
 
 /* Plays the cinematic where the player wakes up and starts looking for his partner */ 
 async function playGameIntro(player) {
+	// Add option for skipping the cinematic.
+	document.getElementById("skip-cinematic").style.display = "block";
 	let eyelids = Array.from(document.getElementsByClassName("board-cover"));
 	eyelids.forEach(
 		eyelid => eyelid.style.display = "block"
